704b8e9b9c7594dae87f1de3df0d1f689aa9a9fd
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-17 19:58:12 -0400

    Made telescope use ripgrep and fd.

--- init.lua
+++ init.lua
@@ -310,6 +310,29 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    file_sorter =  require('telescope.sorters').get_fzy_sorter,
+    prompt_prefix = '> ',
+    color_devicons = true,
+
+    file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
+    extensions = {
+      fzy_native = {
+        override_generic_sorter = false,
+        override_file_sorter = true,
+      },
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
