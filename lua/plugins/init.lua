d7a3578261844f0f68fba12dfbfcd079a190ffac
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-18 06:59:49 -0400

    Remove Noice plugin since it bugs.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -124,16 +124,6 @@ require('lazy').setup({
     opts = {},
   },
 
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
@@ -172,21 +162,3 @@ require('lazy').setup({
     opts = {},
   },
 }, {})
-
-require("noice").setup({
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true,         -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
-  },
-})
